d6e6cf1701a027a3c8d16538e4adf1b3adb99025
Igor Rodrigues <igorar2006@gmail.com>
2025-01-12 21:06:03 -0300

    adicionando sprites .data

--- GAME/SmashitHulk.asm
+++ GAME/SmashitHulk.asm
@@ -1,4 +1,29 @@
 .data
 
+#inclusao das imagens
+
+.include "../DATA/fundo1.data"
+.include "../DATA/fundo2.data"
+.include "../DATA/hulk_ativo.data"
+.include "../DATA/hulk_parado.data"
+.include "../DATA/hulk_pulando.data"
+.include "../DATA/loki_ativo.data"
+.include "../DATA/loki_parado.data"
+.include "../DATA/doende.data"
+.include "../DATA/projetil.data"
+.include "../DATA/portal.data"
+
 .text
+
+# Carrega o fundo1
+FORA:	li t1,0xFF000000	# endereco inicial da Memoria VGA - Frame 0
+	li t2,0xFF012C00	# endereco final 
+	la s1,fundo1		# endereço dos dados da tela na memoria
+	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
+LOOP1: 	beq t1,t2,FORA1		# Se for o último endereço então sai do loop
+	lw t3,0(s1)		# le um conjunto de 4 pixels : word
+	sw t3,0(t1)		# escreve a word na memória VGA
+	addi t1,t1,4		# soma 4 ao endereço
+	addi s1,s1,4
+	j LOOP1			# volta a verificar
 MAIN:
